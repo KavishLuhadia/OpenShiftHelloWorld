95dadd338cc427503fcc76631687c9d18a3ad98d
KavishLuhadia <kavish.luhadia@gmail.com>
2018-10-22 20:21:35 +0100

    OpenShift HelloWorld Application
    OpenShift HelloWorld Application

---
+++ src/main/java/com/javatechclub/HelloWorldOpenShift/HelloWorldOpenShiftApplication.java
@@ -0,0 +1,12 @@
+package com.javatechclub.HelloWorldOpenShift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldOpenShiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldOpenShiftApplication.class, args);
+	}
+}
